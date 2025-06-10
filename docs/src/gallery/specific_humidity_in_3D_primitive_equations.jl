3becdbfe0412a7ac74fc7cadeb5baccd6e27cdd8
Jared Frazier <frazier@iap-kborn.de>
2025-06-10 15:25:42 +0200

    WIP: running default primitive wet model sim

---
+++ docs/src/gallery/specific_humidity_in_3D_primitive_equations.jl
@@ -0,0 +1,25 @@
+module SpecificHumidityExample 
+
+using SpeedyWeather: SpectralGrid, PrimitiveWetModel, OctahedralGaussianGrid, 
+    initialize!, run!, Day, Hour, NetCDFOutput, HumidityOutput, add!,
+    GPU, CPU
+
+function main()
+    spectral_grid = SpectralGrid(
+        trunc=340, Grid=OctahedralGaussianGrid, nlayers=16)
+
+    output = NetCDFOutput(spectral_grid; output_dt=Hour(1))
+    add!(output, HumidityOutput())
+
+    model = PrimitiveWetModel(spectral_grid, output=output)
+    simulation = initialize!(model)
+    run!(simulation, period=Day(6), output=true)
+end 
+
+
+function animate()
+    println("not implemented")
+end 
+
+
+end  
